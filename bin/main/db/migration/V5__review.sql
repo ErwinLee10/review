fd676f88238f65f79c3bde457cc5347ea4cf1bdf
Erwin Lee <erwin@mokapos.com>
2020-04-19 10:09:17 +0800

    add gitignore

---
+++ bin/main/db/migration/V5__review.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE reviews
+   (
+      id          BIGSERIAL    	NOT NULL PRIMARY KEY,
+      userId    BIGSERIAL    	NOT NULL,
+      sellerId    BIGSERIAL   NOT NULL,
+      starRating    BIGSERIAL    	NOT NULL,
+      productId BIGSERIAL        NOT NULL,
+      remarks		VARCHAR(255),
+      created_by  VARCHAR(255),
+      created_dt    DATE,
+      last_updated_by 	VARCHAR(255),
+      last_updated_dt	DATE
+  );
